d3c523668d1352e3c6eaf574fb768ede83dbcd1e
Inder Tecuapetla <inder.tg@gmail.com>
2023-05-30 22:10:27 -0600

    Tested intro_bfast.R with smaller datasets

--- Rscripts/intro_bfast.R
+++ Rscripts/intro_bfast.R
@@ -32,7 +32,7 @@ pixel_ndvi_ts <- ts(as.numeric(pixel_ndvi$ts),
                end = c(2016, 23),
                frequency = 23)
 
-plot(pixel_ndvi_ts)
+plot(pixel_ndvi_ts, ylab="NDVI", main="NDVI (formato INT2S)")
 
 # ---
 
@@ -43,29 +43,20 @@ pixel_nbr_ts <- ts(as.numeric(pixel_nbr$ts),
                       end = c(2016, 23),
                       frequency = 23)
 
-plot(pixel_nbr_ts)
+plot(pixel_nbr_ts, ylab="NBR", main="NBR (formato INT2S)")
 
 ts.plot(pixel_ndvi_ts, pixel_nbr_ts, col = c("blue", "red")) 
 legend("topright", col=c("blue", "red"), 
-       lty=rep(1,2),
+       lty=rep(1,2), bty="n",
        legend=c("NDVI", "NBR"))
 
 # ---
 
-# pixels_df <- data.frame(year=rep(unlist(sapply(2003:2016, function(s) rep(s,23))),
-#                                   2),
-#                         type=c(rep("NDVI", length(pixel_ndvi$ts)), 
-#                                rep("NBR", length(pixel_nbr$ts))),
-#                         value=c(pixel_ndvi_ts, pixel_nbr_ts))
-
-
-pixels_df <- data.frame(year=rep(seq(2003,2016, length=23*14),
+pixels_df <- data.frame(year=rep(seq(2003, 2016, length=23*14),
                                  2),
                         type=c(rep("NDVI", length(pixel_ndvi$ts)), 
                                rep("NBR", length(pixel_nbr$ts))),
                         value=c(pixel_ndvi_ts, pixel_nbr_ts))
-
-
 
 ggplot(pixels_df,    
        aes(x = year,
@@ -74,14 +65,42 @@ ggplot(pixels_df,
   geom_line()
 
 
+# ---
+
+pixel_ndvi_bfast <- bfast(pixel_ndvi_ts,
+                          # h = 23/322,
+                          h = 0.15,
+                          # breaks = 5,
+                          season = "harmonic")
+
+plot(pixel_ndvi_bfast)
+
+# ---
+
+n_iter <- length(pixel_ndvi_bfast$output)
+
+bps <- pixel_ndvi_bfast$output[[n_iter]]$bp.Vt$breakpoints
+
+bps_years <- getYear(bp=bps, start = 2003, end=2016, freq=23)
+
+pixel_dNBR_dNDVI <- get_dNBR_dNDVI(ndvi=pixel_ndvi$ts, 
+                                   nbr=pixel_nbr$ts, 
+                                   breaks = bps)
+
+pixel_vegCondition <- vegCondition(x=pixel_dNBR_dNDVI$dNBR)
+
+sev_df <- data.frame(breaks=bps, years=bps_years, 
+                     dNBR=pixel_dNBR_dNDVI$dNBR, 
+                     vegetationChange=pixel_vegCondition)
 
 
+sev_df
 
+# ---
 
+plot_ndvi_nbr_cps(ndvi=pixel_ndvi, nbr=pixel_nbr, ndvi_bfast=pixel_ndvi_bfast)
 
-
-
-
+  
 
 
 
